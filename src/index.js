43d164b581a374ddfa0666612ae18f13e008115c
llenk <ellenkeal@gmail.com>
2018-08-29 15:34:58 -0500

    setup redux

--- src/index.js
+++ src/index.js
@@ -2,5 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+import logger from 'redux-logger';
+
+import reducer from './redux/reducers';
+
+import rootSaga from './redux/sagas';
+
+const preloadedState = {};
+const middlewares = [];
+const sagaMiddleware = createSagaMiddleware();
+middlewares.push(sagaMiddleware);
+
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
+
+const store = createStore(
+  reducer,
+  preloadedState,
+  applyMiddleware(...middlewares),
+);
+
+sagaMiddleware.run(rootSaga);
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+ document.getElementById('root'));
 
