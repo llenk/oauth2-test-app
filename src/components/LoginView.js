0b458c58f15fd803967e002934b04b4704d7d29b
llenk <ellenkeal@gmail.com>
2018-08-29 15:29:50 -0500

    login form

--- src/components/LoginView.js
+++ src/components/LoginView.js
@@ -4,11 +4,43 @@ import { connect } from 'react-redux';
 class LoginView extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      username: '',
+      password: '',
+    }
+  }
+
+  handleChange = (event) => {
+    const { target } = event;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const { name } = target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleLogin = (event) => {
+    event.preventDefault();
+    console.log(this.state);
   }
 
   render = () => {
     return (
-      <h1>Hi</h1>
+      <div>
+        <h1>Please Log In</h1>
+        <div>
+          <label>Username: </label>
+          <input onChange={this.handleChange} name="username"></input>
+        </div>
+        <div>
+          <label>Password: </label>
+          <input onChange={this.handleChange} name="password" type="password"></input>
+        </div>
+        <div>
+          <button onClick={this.handleLogin}>Submit</button>
+        </div>
+      </div>
     );
   };
 }
