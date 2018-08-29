102ab0f5910d243a38962850fc66840be528dfef
llenk <ellenkeal@gmail.com>
2018-08-29 15:16:30 -0500

    set up router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import {
+  HashRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
+
+import LoginView from './components/LoginView';
+import WallView from './components/WallView';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Router>
+          <Switch>
+            <Route
+              path="/login"
+              component={LoginView}
+            />
+            <Route
+              path="/"
+              component={WallView}
+            />
+            <Route
+              render={() => <h1>404</h1>}
+            />
+          </Switch>
+        </Router>
       </div>
     );
   }
---
+++ src/components/LoginView.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class LoginView extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render = () => {
+    return (
+      <h1>Hi</h1>
+    );
+  };
+}
+
+export default (LoginView);
